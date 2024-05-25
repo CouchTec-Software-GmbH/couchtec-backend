3f0b19fe375822a51e462b7348cb6f873fe69e06
Linus Weigand <linusweigand@protonmail.com>
2024-05-25 14:54:22 +0200

    add tests

--- test.sh
+++ test.sh
@@ -16,6 +16,13 @@ curl -X PUT http://localhost:3000/projects/20 \
 curl -X POST http://localhost:3000/login \
 -H "Content-Type: application/json" \
 -d '{
-  "username": "Linus",
+  "email": "Linus",
   "password": "23456"
+}'
+
+curl -X POST http://localhost:3000/login \
+-H "Content-Type: application/json" \
+-d '{
+  "email": "maxim",
+  "password": "2s3456"
 }'
