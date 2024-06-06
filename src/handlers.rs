e06a1cdca3fb03cf1b8b9e56ebe7e000262c4170
Linus Weigand <linusweigand@protonmail.com>
2024-06-06 12:43:05 +0200

    remove unwrap for better error handling

--- src/handlers.rs
+++ src/handlers.rs
@@ -3,6 +3,7 @@ use std::sync::{Arc, Mutex};
 use crate::db::CouchDB;
 use serde_json::Value;
 use crate::auth::UserManager;
+use crate::email::EmailManager;
 use serde::Deserialize;
 
 pub async fn get_document(db: web::Data<Arc<CouchDB>>, id: web::Path<String>) -> impl Responder {
@@ -26,13 +27,11 @@ pub async fn put_document(db: web::Data<Arc<CouchDB>>, id: web::Path<String>, da
 }
 
 #[derive(Deserialize)]
-pub struct AuthData {
+pub struct LoginData {
     email: String,
     password: String,
 }
 
-pub async fn login(db: web::Data<Arc<CouchDB>>, auth_data: web::Json<AuthData>, user_manager: web::Data<Arc<Mutex<UserManager>>>) -> impl Responder {
-    let mut user_manager = user_manager.lock().unwrap();
 #[derive(Deserialize)]
 pub struct PreRegisterData {
     email: String,
@@ -57,6 +56,11 @@ pub struct ResetPasswordData {
 }
 
 
+pub async fn login(db: web::Data<Arc<CouchDB>>, auth_data: web::Json<LoginData>, user_manager: web::Data<Arc<Mutex<UserManager>>>) -> impl Responder {
+    let mut user_manager = match user_manager.lock() {
+        Ok(manager) => manager,
+        Err(_) => return HttpResponse::InternalServerError().body("Internal Server Error")
+    };
     let user_data = {
         let user = user_manager.get_user(&auth_data.email);
         if user.is_none() {
@@ -69,10 +73,10 @@ pub struct ResetPasswordData {
             user.map(|u| u.clone())
         }
     };
-    if user_data.is_none() {
-        return HttpResponse::NotFound().body("Login failed: User not found");
-    }
-    let user_data = user_data.unwrap();
+    let user_data = match user_data {
+        Some(data) => data,
+        None => return HttpResponse::NotFound().body("Login failed: User not found")
+    };
     match user_manager.sign_in(auth_data.password.clone(), user_data) {
         Ok(session_id) => HttpResponse::Ok().json(format!("{}", session_id)),
         Err(e) => {
