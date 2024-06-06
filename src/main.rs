269d8e6c00d3d2691e81dfb6e54d8230007a8d59
Linus Weigand <linusweigand@protonmail.com>
2024-06-06 12:34:08 +0200

    add email manager to main

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,24 @@ async fn main() -> std::io::Result<()> {
     let db_url = String::from("https://couchdb-app-service.azurewebsites.net");
     let db_username = String::from("admin");
     let db_password = String::from("8RzuxhQ7");
+    dotenv::dotenv().ok();
+    let smtp_email = env::var("SMTP_EMAIL").expect("SMTP_EMAIL must be set");
+    let smtp_password = env::var("SMTP_PASSWORD").expect("SMTP_PASSWORD must be set");
 
     let couchdb = Arc::new(CouchDB::new(db_url, db_username, db_password));
     let user_manager = Arc::new(Mutex::new(auth::UserManager::new()));
+    let email_manager = match EmailManager::new(&smtp_email, &smtp_password) {
+        Ok(manager) => Arc::new(manager),
+        Err(e) => {
+            eprintln!("Failed to create EmailManager: {:?}", e);
+            std::process::exit(1);
+        }
 
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(couchdb.clone()))
             .app_data(web::Data::new(user_manager.clone()))
+            .app_data(web::Data::new(email_manager.clone()))
             .route("/{id}", web::get().to(handlers::get_document))
             .route("/{id}", web::put().to(handlers::put_document))
             .route("/signin", web::post().to(handlers::login))
