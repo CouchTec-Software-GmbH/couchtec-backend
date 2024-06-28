41eec949b6d30f6b9a937da57eed115527737c35
Linus Weigand <linusweigand@protonmail.com>
2024-06-28 15:49:16 +0200

    add chrono for timestamps, add tests

--- test.sh
+++ test.sh
@@ -1,20 +1,3 @@
-curl -X GET http://localhost/api/bf8160da-0639-48ad-9138-bcb988199a66 \
--H "Content-Type: application/json" \
--H "Authorization: Bearer a2838716-7f7d-4433-a1ff-6ffe8a461a29"
-
-
-curl -X PUT http://localhost:3000/projects/20 \
--H "Content-Type: application/json" \
--d '{
-    "name": "Linus",
-    "price": 100,
-    "description": "This is a test",
-    "monitoring": {
-        "enabled": true,
-        "interval": 64200
-    }
-}'
-
 curl -X POST http://localhost/api/pre-register \
 -H "Content-Type: application/json" \
 -d '{
@@ -22,15 +5,61 @@ curl -X POST http://localhost/api/pre-register \
   "password": "23456",
   "newsletter": false
 }'
+
+
 curl -X POST http://localhost/api/register \
 -H "Content-Type: application/json" \
 -d '{
-  "uuid": "9e1bff29-5772-4a87-86c0-18277344c990"
+  "uuid": "e4705207-80a3-4f14-8a8c-5e198db9fe26"
 }'
 
-curl -X GET http://localhost:3000/uuids/linus@couchtec.com
+curl -X POST http://localhost/api/login \
+-H "Content-Type: application/json" \
+-d '{
+  "email": "linus@couchtec.com",
+  "password": "23456"
+}'
 
-curl -X DELETE http://localhost/api/uuids/linus@couchtec.com/lol 
+export SESSION_TOKEN="be467d0a-ed69-4c0d-bfe4-dfdacd58923f"
+
+curl -X DELETE http://localhost/api/user/linus@couchtec.com \
+-H "Content-Type: application/json" \
+-H "Authorization: Bearer $SESSION_TOKEN"
+
+curl -X POST http://localhost/api/logout?id=linus@couchtec.com \
+-H "Content-Type: application/json" \
+-H "Authorization: Bearer $SESSION_TOKEN"
+
+curl -X GET http://localhost/api/uuids/linus@couchtec.com \
+-H "Content-Type: application/json" \
+-H "Authorization: Bearer $SESSION_TOKEN"
+
+
+curl -X PUT http://localhost/api/20 \
+-H "Content-Type: application/json" \
+-H "Authorization: Bearer $SESSION_TOKEN" \
+-d '{
+    "name": "Linus",
+    "price": 100,
+    "description": "This is a pass",
+    "monitoring": {
+        "enabled": true,
+        "interval": 64200
+    }
+}'
+
+curl -X GET http://localhost/api/20 \
+-H "Content-Type: application/json" \
+-H "Authorization: Bearer $SESSION_TOKEN"
+
+curl -X DELETE http://localhost/api/uuids/linus@couchtec.com/20
+
+curl -X POST http://localhost/api/uuids/linus@couchtec.com \
+-H "Content-Type: application/json" \
+-H "Authorization: Bearer $SESSION_TOKEN" \
+-d '{
+  "uuid": "30"
+}'
 
 curl -X DELETE http://localhost/api/user/linus@couchtec.com
 
